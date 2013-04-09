088bf291bcc01aa907136485ce74b7a91a8b03ec
Daniel Lauzon <daniel.lauzon@gmail.com>
2013-04-09 02:47:04 -0400

    readble explained

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 * [DailyJS intro](http://dailyjs.com/2012/12/21/components-holler-gruntstart/)
 * [Node.js blog announcement](http://blog.nodejs.org/2012/12/21/streams2/)
+* [substack / stream-handbook](https://github.com/substack/stream-handbook)
--- readable.js
+++ readable.js
@@ -10,17 +10,38 @@ DigitStream.prototype = Object.create(Readable.prototype, {
 
 function DigitStream(options) {
   Readable.call(this, options);
-  this.totalDigits=10000;
+  // this.totalDigits=10000000;
+  this.totalDigits=30000000;
+  this.totalPushed=0;
+  this.totalRead=0;
 }
 
 function digit(){
   return ''+ (Math.random()*10 | 0);
 }
 
+function readStart(rs) {
+  // console.error('readStart');
+  var count=0;
+  while (true){
+    count++;
+    rs.totalPushed++;
+    var r = rs.push(digit());
+    // console.error('push returned:',r);
+    if(!r) {
+      if (count>1 || (rs.totalPushed %100000)==0){
+        console.error('_push\'d %d digits, tot: %d',count,rs.totalPushed);        
+      }
+      return;
+    }
+  }
+}
+
 DigitStream.prototype._read  = function(size) {
-  // console.log('_read was called size=%d',size);
+  // console.error('_read was called size=%d',size);
   if (this.totalDigits-- > 0){
-    var r = this.push(digit());
+    readStart(this);
+    // var r = this.push(digit());
     // console.error('push returned %j',r);
   } else {
     // console.log('\n');
@@ -30,4 +51,24 @@ DigitStream.prototype._read  = function(size) {
 
 };
 
-new DigitStream().pipe(process.stdout)
+// new DigitStream().pipe(process.stdout)
+var ds = new DigitStream({encoding:'utf8'});
+ds.on('readable', function() {
+  console.error('readable(%d,%d):',ds.totalRead,ds.totalPushed);
+  ds.read(0);
+  console.error('readable(%d,%d):',ds.totalRead,ds.totalPushed);
+});
+ds.pipe(process.stdout)
+if(0)while (true){
+  // var b = ds.read(4096);
+  var b = ds.read();
+  if(b && b.length){
+    ds.totalRead+=b.length;
+    console.error('read(%d):',ds.totalRead);
+    console.log(b);
+  }
+  if(b===null){
+    console.log('done reading:',b);
+    break;
+  }
+}
