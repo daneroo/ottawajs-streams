6c3814b45e79d142c3ba2a03aa59e3621c1dbc3b
Daniel Lauzon <daniel.lauzon@gmail.com>
2013-04-10 12:11:35 -0400

    better piping

--- 04-pipe.js
+++ 04-pipe.js
@@ -2,5 +2,6 @@ var DigitStream = require('./digit-stream');
 var GreenStream = require('./green-stream');
 
 var ds = new DigitStream({encoding:'utf8'});
+var gs = new GreenStream(); 
 
-ds.pipe(new GreenStream());
+ds.pipe(gs);
--- 05-backpressure.js
+++ 05-backpressure.js
@@ -2,11 +2,8 @@ var DigitStream = require('./digit-stream');
 var SlowStream = require('./slow-stream');
 
 
-var chiller = new SlowStream({delay:50});
 
 var ds = new DigitStream({encoding:'utf8',totalDigits: 1<<8 });
-// var ds = process.stdin;
+var slower = new SlowStream({delay:50});
 
-ds.pipe(chiller);
-
-chiller.pipe(process.stdout);
+ds.pipe(slower).pipe(process.stdout);
